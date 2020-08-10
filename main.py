e688c6864768da17a1010b9e466ec3ab81fba8ae
Hugo Chen <harry0731@gmail.com>
2020-08-10 21:53:37 +0800

    api Done

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 from flask import Flask, request, jsonify, redirect, abort
 import configparser
 from urlShortener import urlShortener
+import logging
 
+
+FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('shorturl')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('shorturl.log')
+formatter = logging.Formatter(FORMAT)
+fh.setFormatter(formatter)
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
 
 config = configparser.ConfigParser()
 config.read("config.ini") 
 
 app = Flask(__name__)
 app.debug = True
-shortener = urlShortener(config)
+shortener = urlShortener(config, logger)
 
 @app.route("/shortURL", methods=['POST'])
 def shorten_request():
 	"""
 	Get the shorten request from client.
 	"""
-	url = request.get_data()
-	chk_url_res = shortener.valid_url(url)
+	data = request.get_json()
+	chk_url_res = shortener.valid_url(data["url"])
 	if chk_url_res["State"] != "Success":
 		return jsonify(chk_url_res)
 
-	result = shortener.use_rand_key(url)
+	result = shortener.generate_shorturl(data["url"])
 	if result["State"] != "Success":
 		return jsonify(result), 400
 	return jsonify(result), 200
@@ -30,7 +41,7 @@ def redirect_to_url(url_key):
 	"""
 	Check the url_key is in DB, redirect to original url.
 	"""
-	url = shortener.get_from_redis(url_key)
+	url = shortener.get_url(url_key)
 	if url is None:
 		return jsonify({"State": "Failed", 
 						"Info": "url_key: '%s' is not in database" % (url_key)}), 404
@@ -46,4 +57,4 @@ def page():
 
 if __name__ == '__main__':
 	# Test with Flask development server
-	app.run(host='localhost', port=8080)
+	app.run(host='localhost', port=8080)	
--- urlShortener/__init__.py
+++ urlShortener/__init__.py
@@ -6,73 +6,98 @@ import pymongo
 
 
 class urlShortener:
-    def __init__(self, config):
+    def __init__(self, config, logger):
         self.config = config
+        self.logger = logger
         self.redis = redis.StrictRedis(
             host=self.config["redis"]["host"],
-            port=self.config["redis"]["port"],
+            port=int(self.config["redis"]["port"]),
             decode_responses=True)
         self.mongo = pymongo.MongoClient(
             host=self.config["mongodb"]["host"],
             port=int(self.config["mongodb"]["port"]))
-        self.mdb = self.mongo[self.config["mongodb"]["collection"]]
+        self.mdb = self.mongo[self.config["mongodb"]["collection"]][self.config["mongodb"]["collection"]]
+        self.logger.debug("urlShortener initial")
     
     def _short(self, url, count):
         """
         MD5 hash and take first 40bit encdoe with base62
         """
         if count + 3 < len(hashlib.md5(url.encode('utf-8')).digest()):
-            return base62.encodebytes(hashlib.md5(url.encode('utf-8')).digest()[count:count+3])
+            result = base62.encodebytes(hashlib.md5(url.encode('utf-8')).digest()[count:count+3])
+            self.logger.debug(f"shorten hash url: {url}, count: {count}, result: {result}")
+            return result
         else:
+            self.logger.debug("shorten hash url count > md5 bits")
             return None
 
     def valid_url(self, url):
         """
         Check with too long string or missed protocol
         """
-        if (len(url) > self.config["DEFAULT"]["MAX_URL_LEN"]):
+        if (len(url) > int(self.config["DEFAULT"]["MAX_URL_LEN"])):
+            self.logger.debug(f"url: {url} length illigal")
             return {"State":"Failed", 
                     "Error_msg":"Please don't use url longer than " + str(self.config["DEFAULT"]["MAX_URL_LEN"]) + " character."}
-        parsed_url = parse.urlparse(url.decode())
+        parsed_url = parse.urlparse(url)
         if parsed_url.scheme == '':
+            self.logger.debug(f"url: {url} format illigal")
             return {"State":"Failed", 
                     "Error_msg":"Please use correct protocol such as 'http', 'https' or 'ftp'."}
         return {"State":"Success"}
 
-    def get_from_redis(self, url_key):
+    def get_url(self, url_key):
+        self.logger.debug(f"get url, url_key: {url_key}")
+        result = self._get_from_redis(url_key)
+        if result == None:
+            result = self._get_from_mongo(url_key)
+        return result
+
+    def _get_from_redis(self, url_key):
         """
         Check the given url_key is in cache or not
         """
         return self.redis.get(url_key)
 
-    def get_from_mongo(self, url_key):
+    def _get_from_mongo(self, url_key):
         """
         Check the given url_key is in database or not
         """
         return self.mdb.find_one({"url_key": url_key})
 
-
     def _set_to_redis(self, url_key, url):
         """
-        Set the given key-val to db, if failed return error message.
+        Set the given key-val to cache
         """
         try:
             self.redis.set(url_key, url)
-            return {"State":"Success"}
+            return True
         except:
+            return False
+    
+    def _set_to_mongo(self, url_key, url):
+        """
+        Set the given key-val to db, if failed return error message.
+        """
+        data = {"url_key": url_key, "url": url}
+        self.logger.debug(f"insert data to mongodb: {data}")
+        try:
+            self.mdb.insert_one(data)
+            r = self._get_from_mongo(url_key)
+            print(r)
+            if r != None and r["url"] == url:
+                self.logger.debug("insert data to mongodb success")
+                return {"State":"Success"}
+            else:
+                self.logger.debug("insert data to mongodb failed")
+                return {"State":"Failed", 
+                        "Error_msg": "Insert to database failed"}
+        except:
+            self.logger.debug("insert data to mongodb failed")
             return {"State":"Failed", 
                     "Error_msg": "Insert to database failed"}
 
-    def _is_collision(self, url_key, url):
-        """
-        Check if the given url_key has collision
-        """
-        assert self.redis.get(url_key) is not None
-        if self.redis.get(url_key) != url:
-            return True
-        return False
-
-    def use_rand_key(self, url):
+    def generate_shorturl(self, url):
         """
         The work flow of general usage.
         """
@@ -81,25 +106,40 @@ class urlShortener:
         count = 0
         while url_key == None:
             # generate short url
-            # check if existed
-            # check in collisio
             url_key = self._short(url, count)
-            # Check if the generated key has been used
-            if self.get_from_mongo(url_key) is not None:
-                # key is in db, check collision
-                if self._is_collision(url_key, url):
-                    # collision, insert it and replace the old one.
-                    set_res = self._set_to_redis(url_key, url)
-                    if set_res["State"] != "Success":
-                        return set_res
+            if url_key != None:
+                # Check if the generated key has been used
+                collision_check = self._get_from_mongo(url_key)
+                if collision_check != None:
+                    # key is in db, check collision
+                    if collision_check["url"] == url:
+                        self.logger.debug("URL already existed")
+                        return {"State":"Sucess", 
+                                "short_url":"%s/%s" % (self.config["DEFAULT"]["SERVER_URL_PREFIX"], url_key)} 
+                    else:
+                        # collision occured generate new hah
+                        count += 1
+                        continue
                 else:
-                    # TODO
-                    pass
-        else:
-            # key is not in db, insert it.
-            set_res = self._set_to_redis(url_key, url)
-            if set_res["State"] != "Success":
-                return set_res
+                    # key is not in db, insert it.
+                    set_res = self._set_to_mongo(url_key, url)
+                    if set_res["State"] != "Success":
+                        # insert faild try with new hash
+                        self.logger.debug("insert to db faild")
+                        count += 1
+                        continue
+                    else:
+                        # insert succe, put data in cache
+                        cache = self._set_to_redis(url_key, url)
+                        self.logger.debug(f"Cache Success: {cache}")
+                        return {"State":"Sucess", 
+                                "short_url":"%s/%s" % (self.config["DEFAULT"]["SERVER_URL_PREFIX"], url_key)}
+            else:
+                # Used all md5 bits
+                self.logger.debug("Used all md5 bits")
+                break
+        return {"State":"Failed", 
+                "Error_msg": "Couldn't find appropriate short url"}
+        
 
-        return {"State":"Sucess", 
-                "short_url":"%s/%s" % (self.config["DEFAULT"]["SERVER_URL_PREFIX"], url_key)} 
+        
